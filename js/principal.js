b07eedcd1ff0db83472d50914ff119177fa6b33a
KathLouise <kathe.nightwish@gmail.com>
2019-02-08 14:52:07 -0200

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -41,4 +41,7 @@ for(var i = 0; i < pacientes.length; i++){
     }
 }
 
-
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function() {
+    console.log("teste");
+});
